66cd4c9138a9b368afa29ee52b14aa40dad423f1
Lucy <133673971+lucyocean@users.noreply.github.com>
2023-08-02 19:16:07 -0400

    continue to figure out way for gamil automation

--- error_checking.py
+++ error_checking.py
@@ -3,6 +3,7 @@ import subprocess
 import smtplib
 from email.message import EmailMessage
 import os
+import ssl
 # Coming up with an Error checking function
 def check_for_errors():
     # make an try block
@@ -29,28 +30,55 @@ def send_email(subject, body):
     em['To'] = email_receiver
     em['Subject'] = subject
     em.set_content(body)
+    
+    # we are first making an default context
+    context = ssl.create_default_context()
+
+    # since there is connection error, we need to make an try ans except block
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465,context=context) as smtp:
+            # we are going throught the process of login
+            smtp.login(email_sender, email_password)
+            smtp.send_message(em)
+            print("Email sent successfully!")
+    # check to see if there are connection errors
+    except ConnectionRefusedError as e:
+        print("Connection refused. Check network/firewall settings.")
+    # check to see if there are authentication errors
+    except smtplib.SMTPAuthenticationError as e:
+        print("Authentication error. Check email credentials.")
+    # check to see if there are any other type of errors
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
 
     # taking care of the context
-    context = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    context.login(email_sender,email_password)
-    context.send_message(em)
-    context.quit()
+    # context = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    # context.login(email_sender,email_password)
+    # context.send_message(em)
+    # context.quit()
 
 # defining the main function
+# revise the mian function
 def main():
-    # Checking for errors
-    error_message = check_for_errors()
+    subject = "Test Email"
+    body = 'This is a test email.'
+    # then, we are senting the email
+    send_email(subject, body)
+    # # Checking for errors
+    # error_message = check_for_errors()
 
     # checking if there is error
-    if error_message:
-        # then, we are sending email if errors are detected
-        subject = "Error Detected in Program"
-        body = f"Error message:\n{error_message}"
-        send_email(subject, body)
-        print("Error detected. Email sent.")
-    else:
-        print("No errors detected.")
+    # if error_message:
+    #     # then, we are sending email if errors are detected
+    #     subject = "Error Detected in Program"
+    #     body = f"Error message:\n{error_message}"
+    #     send_email(subject, body)
+    #     print("Error detected. Email sent.")
+    # else:
+    #     print("No errors detected.")
 
 
-if __name__ == "__error_checking":
-    main()
+main()
