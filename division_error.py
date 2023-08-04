767097f445f54fbb69a2e00cd81c4f097f2dc7a7
Lucy <133673971+lucyocean@users.noreply.github.com>
2023-08-04 18:59:30 -0400

    intentionally introducing error for email test

--- division_error.py
+++ division_error.py
@@ -11,3 +11,6 @@ def division_zero(number1,number2):
 # we are calling the function
 division_zero(10, 2) # No error, result will be print
 division_zero(10, 0)# The Diviision by zero error will occur
+
+# Now, We need to Introduce an intentional error to prevent the script from running successfully
+some_variable = non_existent_variable
