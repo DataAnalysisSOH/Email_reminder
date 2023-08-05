56be1f863900be2d1311944a2ec1d6b039c0448d
Lucy <133673971+lucyocean@users.noreply.github.com>
2023-08-04 20:22:37 -0400

    Successfully senting error email

--- main.py
+++ main.py
@@ -1,61 +1,85 @@
 # Importing the require library
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
+import logging
+import ssl
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 
-# We are Creating an variable to link to the client service file
-SERVICE_ACCOUNT_FILE = 'Credential.json'
-credentials = service_account.Credentials.from_service_account_file(
-    filename= SERVICE_ACCOUNT_FILE,
-    scopes = ['https://mail.google.com/'],
-    subject = 'lucy.wang@soundofhope.org'
-)
+# doing an logging
+logging.basicConfig(filename='error_log.txt',level=logging.ERROR),
+format = '%(asctime)s - %(levelname)s - %(message)s'
 
-# defining the main function
-def main():
-    service_gmail = build('gmail','v1',credentials=credentials)
-    response = service_gmail.users().getProfile(userId='me').execute()
-    print(response)
+# defining an function to send email
+def send_email(subject, body):
+    # taking care of the try block
+    try:
+         # Email configuration
+        email_sender = 'lucy.wang@soundofhope.org'
+        email_password = 'Wang233579762'
+        email_receiver = 'lucy.wang@soundofhope.org'
+
+        # taking care of the message variables
+        msg = MIMEText(error_message,'plain','utf-8')
+        msg['From'] = email_sender
+        msg['To'] = email_receiver
+        msg['Subject'] = Header(subject, 'utf-8').encode()
+        # we are attaching to the main body
+        #message.attach(MIMEText(body, 'plain'))
+
+        # Set the plain text content of the email
+        # plain_text = error_message
+        # text_part = MIMEMultipart(plain_text, 'plain', 'utf-8')
+
+        # # We are Attach the text part to the message
+        # msg.attach(text_part)
+
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        # Send the error email
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+             smtp.login(email_sender,email_password)
+             smtp.sendmail(email_sender, email_receiver, msg.as_string())
+        print("Email sent successfully")
+    except Exception as e:
+         print("Failed to send error email:",e)
+             
+
+    # # taking care of the first try block
+    # try:
+    #     # taking care of the server
+    #     server = smtplib.SMTP('smtp.example.com',587)
+    #     server.starttls()
+    #     server.login(send_email, password)
+    #     # sending the email
+    #     server.sendmail(sender_email,receiver_email,message.as_string())
+    #     # make sure to quit the server
+    #     server.quit()
+    #     print("Email sent successfully")
+    # except Exception as e:
+    #     print("Failed to send email", str(e))
 
 
-# entry point of the function
+# taking care of the entry point of the program
 if __name__ == "__main__":
-     main()
+      # get the path of the python script
+    script_path = 'division_error.py'
 
+      # taking care of the second try block:
+    try:
+           # We are Reading the content of the script file
+            with open(script_path,'r') as script_file:
+                script_code = script_file.read()
 
-
-
-
-
-
-
-# # Importing the require library
-# import os
-# from email.message import EmailMessage
-# import ssl
-# import smtplib
-# # We are Initialized few variable for email senting
-# email_sender = 'lucy.wang@soundofhope.org'
-# email_password = os.environ.get("EMAIL_PASSWORD")
-# email_receiver = 'lucy.wang@soundofhope.org'
-
-# # defining the subject
-# subject = 'Check out the email reminder'
-# body = """
-# Starting to test whether we are able to sent email successfully through python.
-# """
-
-# # declare EmailMessage object
-# em = EmailMessage()
-# em['From'] = email_sender
-# em['To'] = email_receiver
-# em['Subject'] = subject
-# # We are taking care of the body
-# em.set_content(body)
-
-# # taking care of the security
-# context = ssl.create_default_context()
-
-# # using smtplib to sent the email
-# with smtplib.SMTP_SSL('smtp.gmail.com', 456, context=context) as smtp:
-#     smtp.login(email_sender,email_password)
-#     smtp.sentmail(email_sender,email_receiver,em.as_string())
+                # we are executing the script
+                exec(script_code)
+            # print out the scripts are running successfully
+            print("Script executed successfully")
+        # taking care of the excpetion block
+    except Exception as e:
+         error_message = f"Error in {script_path}: {str(e)}"
+         # we are logging the error
+         logging.error(error_message)
+         # then, we are senting the email with the error message
+         send_email("Error Notification", error_message)
